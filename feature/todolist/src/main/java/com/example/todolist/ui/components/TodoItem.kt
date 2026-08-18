965035decee9d816d9951563c29e6465324819d4
jSY49 <jseung49@gmail.com>
2026-08-18 17:10:09 +0900

    fix(feature-todolist): TodoItem 패키지 경로 오류 수정

--- feature/todolist/src/main/java/com/example/todolist/ui/components/TodoItem.kt
+++ feature/todolist/src/main/java/com/example/todolist/ui/components/TodoItem.kt
@@ -1,4 +1,4 @@
-package com.example.todoapplication.ui.components
+package com.example.todolist.ui.components
 
 import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.animation.core.tween

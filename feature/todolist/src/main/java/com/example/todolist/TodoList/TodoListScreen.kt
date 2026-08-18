0327b88fc7199b26cca5bc426aa57e7b831efa20
jSY49 <jseung49@gmail.com>
2026-08-18 17:06:53 +0900

    feat(feature-todolist): 할일 아이템 데이터클래스 추가

---
+++ feature/todolist/src/main/java/com/example/todolist/TodoList/TodoListScreen.kt
@@ -0,0 +1,4 @@
+package com.example.todolist.TodoList
+
+class TodoListScreen {
+}
---
+++ feature/todolist/src/main/java/com/example/todolist/model/Todo.kt
@@ -0,0 +1,7 @@
+package com.example.todolist.model
+
+data class Todo(
+    val id: String,
+    val title: String,
+    val isDone: Boolean
+)

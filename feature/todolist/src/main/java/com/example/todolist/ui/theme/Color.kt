4aeae68b5613754b42b3d469fb704ee10c152643
jSY49 <jseung49@gmail.com>
2026-08-20 15:58:20 +0900

    feat(feature-todolist): 기본 배경 색상 추가

--- feature/todolist/src/main/java/com/example/todolist/ui/theme/Color.kt
+++ feature/todolist/src/main/java/com/example/todolist/ui/theme/Color.kt
@@ -12,5 +12,6 @@ val Pink40 = Color(0xFF7D5260)
 
 val baseBlue = Color(0xFF3F51B5)
 val item_bg = Color(0xFF1E1E1E)
+val bg = Color(0xFF101010)
 val title_color = Color(0xFFF2F2F2)
 val sub_title_color = Color(0xFF9A9A9E)

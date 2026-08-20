d3d55c10ec4a1d0e3836c1a40031d657c2c27ed0
jSY49 <jseung49@gmail.com>
2026-08-20 17:07:40 +0900

    feat(app): 모듈 이동 네비게이션 추가

---
+++ app/src/main/java/com/example/myandroidplayground/NaviButtons.kt
@@ -0,0 +1,45 @@
+package com.example.myandroidplayground
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun NaviButtons(modifier : Modifier = Modifier,
+                onTodoListClick: () -> Unit
+) {
+
+    Surface(
+        modifier = Modifier.fillMaxSize(),
+        color = Color.Black
+    ) {
+        var buttonModifier = modifier.padding(8.dp)
+        var buttonColor = ButtonDefaults.buttonColors(
+            containerColor = Color(0xFF2A2A2A),   // 배경색
+            contentColor = Color.White             // 텍스트/아이콘 색
+        )
+
+        LazyVerticalGrid(
+            modifier = modifier.background(Color.Black),
+            columns = GridCells.Fixed(3)
+        ) {
+            item {
+                Button(
+                    modifier = buttonModifier,
+                    colors = buttonColor,
+                    onClick = onTodoListClick
+                ) { Text("TODO") }
+            }
+        }
+    }
+}
